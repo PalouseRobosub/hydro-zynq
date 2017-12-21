04cb96ab89813a57459e104e111efc4eb488519a
James Irwin <irwineffect@gmail.com>
2017-12-21 12:07:51 -0800

    added debug console

---
+++ scripts/debug_console.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+import socket
+import argparse
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Displays console output from hydro-zynq board")
+    parser.add_argument('--hostname', type=str, default='192.168.0.250', help='Specifies the hostname to bind to')
+    args = parser.parse_args()
+
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.bind((args.hostname, )) #TODO: add port
+
+    while True:
+        data = sock.recv(1024)
+        print data
