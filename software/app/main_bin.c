63d326db92f2fdab3342e9fb070d53870654518d
Ryan Summers <summers.ryan.m@gmail.com>
2018-03-21 04:21:44 -0700

    Added in trivial read of first few ADC samples

--- software/app/main_bin.c
+++ software/app/main_bin.c
@@ -306,6 +306,12 @@ result_t go()
     dbprintf("ADC samples per packet: %d\n", adc.regs->samples_per_packet);
 
     /*
+     * Read the first sample from the ADC and ignore it - the ADC always has an
+     * invalid measurement as the first reading.
+     */
+    AbortIfNot(record(&dma, samples, adc.regs->samples_per_packet, adc), fail);
+
+    /*
      * Bind the command port, data stream port, and the result output port.
      */
     udp_socket_t command_socket, data_stream_socket, result_socket, xcorr_stream_socket, silent_request_socket;
